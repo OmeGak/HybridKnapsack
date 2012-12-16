4e04e060b78777dc09d61f99329d225474190fb0
Ome Gak <omegak@gmail.com>
2012-12-16 17:16:45 +0100

    Hello World

---
+++ src/HybridKnapsack.java
@@ -0,0 +1,12 @@
+
+public class HybridKnapsack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
