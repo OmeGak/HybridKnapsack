7db21488875378fcf7f8f9b6cc6368ae6dd3181b
Ome Gak <omegak@gmail.com>
2012-12-27 18:00:47 +0100

    Main class ready

--- src/HybridKnapsack.java
+++ src/HybridKnapsack.java
@@ -4,13 +4,29 @@
  * @author omegak
  */
 public class HybridKnapsack {
-
+	
+	/** Usage of HybridKnapsack. */
+	private static final String USAGE = "usage: HybridKnapsack [path]";
+	
+	/** Path where the instances of the knapsack problem are located. */
+	private static String path = "instances";
+	
 	/**
-	 * @param args
+	 * Main method of the program. It launches the UI.
+	 * 
+	 * @param args Arguments passed to be processed.
 	 */
 	public static void main(String[] args) {
-		// TODO Main calls must be placed here
-		System.out.println("Hello World");
+		
+		// Arguments handling
+		if (args.length > 0) {
+			path = args[0];			
+		} else if (args.length > 1) {
+			System.out.println(USAGE);
+		}
+		
+		// User Interface execution
+		UI.run(path);
 	}
 
 }
