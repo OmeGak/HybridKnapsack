fde8232494cd28a7ffcfb40159046e4afd53bc7a
root <root@napoleon-DOT-SE.(none)>
2013-01-20 20:58:23 +0100

    Update Sunday 20:58 Jorge

--- src/heuristics/TabuSearch.java
+++ src/heuristics/TabuSearch.java
@@ -12,23 +12,20 @@ import problem.Knapsack;
  */
 public class TabuSearch extends Heuristic {
 
-	/**Tabu list with the worst elements of the KS*/
+	/**Tabu list with the previous best elements of the KS*/
 	private Queue <Knapsack> tabuList;
-	
-	/** List of the possible candidates in the neighborhood*/
-	private ArrayList <Knapsack> candidateList;
 	
 	/** Knapsack that contains the best solution*/
 	private Knapsack bestSol;
 
 	/** Knapsack that contains the best solution*/
 	private Knapsack sCandidate;
-
-	/**Index to iterates the knapsack*/
-	private int sTabulist=0;
 	
 	/** It contains the closed neighborhood*/
 	private ArrayList <Knapsack> neighborhood;
+	
+	/** It contains the size of the tabu list*/
+	private int tabuListSize;
 	
 	
 		/**
@@ -38,52 +35,40 @@ public class TabuSearch extends Heuristic {
 		 * */
 		
 		
-		public TabuSearch(){
+		public TabuSearch(int tlist){
 					
-			/**Nothing to do*/
+			this.tabuListSize=tlist;
 		}
 
 		@Override
 		public Knapsack executeOnce(Knapsack knapsack) {
 			
-			while(!bestSol.isFull())
-			{
+		
 				/**Variables of control*/
 				int i = 0;
-				int j = 0;
 				int n = 0;
-				
-				/**The candidate list is empty at first instance*/
-				candidateList = null;		
-				
+								
 				/**It obtains a r index to give back a key to preserve the neighborhood*/
 				n =generateRandomNumberOfNeighbours();
 				
 				/**It creates a random neighborhood*/
-				
-				
-				
-					neighborhood.add(generateNeighbour(bestSol));
-				
-				
-				
+				for(i=0;i<n;i++){
+					sCandidate=generateNeighbour(bestSol);
+							
 				/**This part compares if the candidate is included in the tabuList*/
-				for(i=0;i<n;i++)
-				{
-									
+				
 					if(tabuList.isEmpty()){
-						candidateList.add(sCandidate);			
+						tabuList.add(sCandidate);			
 					}
 					/**If the tabu list is not empty but not contains
 					 * the next candidate, include the candidate in the 
 					 * candidate list*/
 					else{
 						if(!tabuList.contains(sCandidate)){
-							candidateList.add(sCandidate);
-							
+							tabuList.add(sCandidate);
 						}						
 					}
-				}
+				
 				
 				/**This part evaluates the different solutions
 				 * If the candidate solution is better than the previous one
@@ -95,17 +80,12 @@ public class TabuSearch extends Heuristic {
 					bestSol = new Knapsack(sCandidate);
 					
 					/**The tabuList just can be get twelve partial solutions*/
-					if(tabuList.size()>rounds)
-					{
+					if(tabuList.size()>tabuListSize)
 						tabuList.remove();
-						
-					}
-					
-				}
 					
 			}
 					
-		
+			}
 			return bestSol;
 		}
 				
