3542413d27f37bc689e9f1ef02e13fd34074b3f9
Ome Gak <omegak@gmail.com>
2013-01-20 20:27:05 +0100

    LocalSearch completed

--- src/heuristics/LocalSearch.java
+++ src/heuristics/LocalSearch.java
@@ -1,25 +1,35 @@
 package heuristics;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import problem.Element;
+import problem.Evaluator;
 import problem.Knapsack;
 
 /**
- * TODO add javadoc
+ * Local search heuristic.
+ * 
  * @author omegak
- *
  */
 public class LocalSearch extends Heuristic {
-
+	
 	@Override
-	public Knapsack executeOnce(Knapsack knapsack) {
-		HashMap<Integer, Element> insertedElements = knapsack.copyInsertedElements();
+	public Knapsack executeOnce(Knapsack knapsack) {		
+		int neighbours = generateRandomNumberOfNeighbours();
+		Knapsack best = null;
 		
-		for (Integer key : insertedElements.keySet()) {
-			// TODO hacer y tal
+		for (int i=0; i<neighbours; i++) {
+			Knapsack candidate = generateNeighbour(knapsack);
+			
+			if (best == null) {
+				best = candidate;
+			} else if (candidate.evaluate() > best.evaluate()) {
+				best = candidate;
+			}
 		}
 		
-		return knapsack;
+		return best;
 	}
 }
