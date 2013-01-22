8c5d1fd20e3c1b1fe5fc7d86b20e41f64cccf7c4
Ome Gak <omegak@gmail.com>
2013-01-22 16:06:58 +0100

    main now allows to run batch operations

--- src/HybridKnapsack.java
+++ src/HybridKnapsack.java
@@ -1,3 +1,4 @@
+
 /**
  * HybridKnapsack main class.
  * 
@@ -11,6 +12,9 @@ public class HybridKnapsack {
 	/** Path where the instances of the knapsack problem are located. */
 	private static String path = "instances";
 	
+	/** Number of tries for running each instance. */
+	private static int tries = 30;
+	
 	/**
 	 * Main method of the program. It launches the UI.
 	 * 
@@ -19,14 +23,17 @@ public class HybridKnapsack {
 	public static void main(String[] args) {
 		
 		// Arguments handling
-		if (args.length > 0) {
-			path = args[0];			
-		} else if (args.length > 1) {
+		if (args.length == 0) {
+			UI.run(path);
+		} else if (args.length == 1) {
+			path = args[0];
+			UI.run(path);
+		} else if (args.length == 2) {
+			path = args[0];
+			tries = Integer.parseInt(args[1]);
+			Launcher.launchBatch(path, tries);
+		} else if (args.length > 2) {
 			System.out.println(USAGE);
 		}
-		
-		// User Interface execution
-		UI.run(path);
 	}
-
 }
