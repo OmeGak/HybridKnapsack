c468ab4af586ce5a1fc046607077cdfbf9168ef1
Ome Gak <omegak@gmail.com>
2013-01-23 17:13:36 +0100

    TabuSearch fixed

--- src/heuristics/TabuSearch.java
+++ src/heuristics/TabuSearch.java
@@ -3,7 +3,6 @@ package heuristics;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import problem.Evaluator;
 import problem.Knapsack;
 
 /**
@@ -15,12 +14,6 @@ public class TabuSearch extends Heuristic {
 
 	/** Tabu list with the previous best elements of the KS */
 	private Queue<Knapsack> tabuList;
-
-	/** Knapsack that contains the best solution */
-	private Knapsack bestSol;
-
-	/** Knapsack that contains the best solution */
-	private Knapsack sCandidate;
 
 	/** It contains the size of the tabu list */
 	private int tabuListMaxSize;
@@ -40,37 +33,53 @@ public class TabuSearch extends Heuristic {
 		int neighbours = generateRandomNumberOfNeighbours();
 		Knapsack best = null;
 		
-		/** In first instance, the best solution is added by the Agent */
-		bestSol = knapsack;
-		
 		/** It creates a random neighborhood */
 		for (int i = 0; i < neighbours; i++) {
-			sCandidate = generateNeighbour(knapsack);
+			Knapsack candidate = generateNeighbour(knapsack);
 			
-			if (!tabuList.contains(sCandidate)) {
-				tabuList.add(sCandidate);
+			if (!isInTabuList(candidate)) {
+				if (best == null) {
+					best = candidate;
+				} else if (candidate.compareWith(knapsack) >= 0) {
+					best = candidate;
+				}
 			}
+		}
+			
+		if (best != null) {
+			addInTabuList(best);
+			return best;
+		} else {
+			return knapsack;
+		}
+	}
+	
+	/**
+	 * Adds in the tabu list a given knapsack. If the tabu list is full the oldest knapsack is removed.
+	 * 
+	 * @param k The knapsack to be stored.
+	 */
+	private void addInTabuList(Knapsack k) {
+		tabuList.offer(k);
+		
+		if (tabuList.size() > tabuListMaxSize) {
+			tabuList.poll();
+		}
+	}
 
-			/**
-			 * This part evaluates the different solutions If the candidate
-			 * solution is better than the previous one it must be changed and
-			 * added to the tabu list
-			 * */
-			if (sCandidate.evaluate() > knapsack.evaluate()) {
-
-				tabuList.add(sCandidate);
-				bestSol = new Knapsack(sCandidate);
-
-				/** The tabuList just can be get twelve partial solutions */
-				if (tabuList.size() > tabuListMaxSize)
-					tabuList.remove();
-
-			}
-			if (bestSol.evaluate() == Evaluator.INVALID) {
-				bestSol = knapsack;
+	/**
+	 * Determines whether a given knapsack already exists within the tabu list.
+	 * 
+	 * @param k The knapsack to be checked if it already exists.
+	 * @return TRUE if present, FALSE otherwise.
+	 */
+	private boolean isInTabuList(Knapsack k) {
+		for (Knapsack tabu : tabuList) {
+			if (k.equals(tabu)) {
+				return true;
 			}
 		}
 		
-		return bestSol;
+		return false;
 	}
 }
